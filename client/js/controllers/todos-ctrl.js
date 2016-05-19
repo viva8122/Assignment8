536ffcd922410e13738d053684340ae5827e091f
viva8122 <viva8122@gmail.com>
2016-05-19 00:48:14 -0700

    Make it better

--- client/js/controllers/todos-ctrl.js
+++ client/js/controllers/todos-ctrl.js
@@ -35,4 +35,25 @@ angular.module('todoController', [])
 			}
 		};
 
+		// CLEAR ==================================================================
+		// when submitting the clear form, send the text to the node API
+		$scope.clearTodo = function() {
+
+			// validate the formData to make sure that something is there
+			// if form is empty, nothing will happen
+			if ($scope.formData.text != undefined) {
+				$scope.loading = true;
+
+				// call the create function from our service (returns a promise object)
+				Todos.create($scope.formData)
+
+					// if successful creation, call our get function to get all the new todos
+					.success(function(data) {
+						$scope.loading = false;
+						$scope.formData = {}; // clear the form so our user is ready to enter another
+						$scope.todos = data; // assign our new list of todos
+					});
+			}
+		};
+
 	}]);
